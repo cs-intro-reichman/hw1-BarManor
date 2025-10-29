cedd1956ce90f9e606aa6d4b2122f53a53827893
BarManor <bar.manor01@post.runi.ac.il>
2025-10-29 10:21:33 +0200

    finished FVCalc

--- FVCalc.java
+++ FVCalc.java
@@ -2,9 +2,9 @@
 public class FVCalc {
 	public static void main(String[] args){
 		// Replace this comment with your code
-		int numyears = Integer.parseInt(args[0]);
-		int CurrentValue = Integer.parseInt(args[1]);
-		double IntrestRate = Double.parseDouble(args[2]);
+		int CurrentValue = Integer.parseInt(args[0]);
+		double IntrestRate = Double.parseDouble(args[1]);
+			int numyears = Integer.parseInt(args[2]);
 		double FutureValue = CurrentValue * Math.pow(1 + IntrestRate / 100.0, numyears);
 		System.out.println("After " + numyears + " years, " + CurrentValue + "$ saved at " + IntrestRate + "% will yield " + (int)(FutureValue) + "$");
 
