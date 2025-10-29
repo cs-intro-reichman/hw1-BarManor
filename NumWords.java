b4322efd2e87957a6091f8af97edb525add2449d
BarManor <bar.manor01@post.runi.ac.il>
2025-10-29 10:39:54 +0200

    finished NumWords

--- NumWords.java
+++ NumWords.java
@@ -2,7 +2,7 @@
 public class NumWords {
 	public static void main(String args[]) {
 int n = Integer.parseInt(args[0]);
-int hundreds = (n%1000)/100;
+int hundreds = n/100;
 int tens = (n%100)/10;
 int units = n % 10;
 System.out.print(hundreds + " hundreds, ");
