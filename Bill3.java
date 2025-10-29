9b1c0412a2caad158cb82b8bd87e08cf624b701f
BarManor <bar.manor01@post.runi.ac.il>
2025-10-29 10:33:08 +0200

    finished  NumWords

--- Bill3.java
+++ Bill3.java
@@ -6,7 +6,7 @@ public class Bill3{
 		String name2 = args[1];
 		String name3 = args[2];
 		int billAmount = Integer.parseInt(args[3]);
-		double splitAmount = Math.ceil((double)billAmount / 3);
+		double splitAmount = Math.ceil((double)billAmount / 3);// a double which rounds up the split amount to the nearest integer
 		System.out.println( "Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + splitAmount + " Shekels each.");
 					
 	}
--- NumWords.java
+++ NumWords.java
@@ -5,8 +5,8 @@ int n = Integer.parseInt(args[0]);
 int hundreds = (n%1000)/100;
 int tens = (n%100)/10;
 int units = n % 10;
-System.out.print(hundreds + " hundred ");
-System.out.print(tens + " ten ");
-System.out.print(units + " unit");
+System.out.print(hundreds + " hundreds, ");
+System.out.print(tens + " tens,");
+System.out.print(units + " ones.");
 }
 }
